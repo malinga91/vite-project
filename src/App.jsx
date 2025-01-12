43f95fd4c1a71f2c16a6c766dc70efa8a39a3eef
Suraj Malinga <malinga91@gmail.com>
2025-01-13 00:16:47 +0530

    Removted description text

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,6 @@ function App() {
         </button>
         
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
